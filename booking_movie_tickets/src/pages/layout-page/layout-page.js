e113e95dfc0f2ca668385f9824ea5b7fcc1c0734
Brodich Vlad <brodich_vlad@ukr.net>
2023-08-04 10:20:54 +0300

    homework_react_8_sinema 04.08.2023 Виправлення помилок після тестування

--- booking_movie_tickets/src/pages/layout-page/layout-page.js
+++ booking_movie_tickets/src/pages/layout-page/layout-page.js
@@ -2,6 +2,8 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import Loader from '../../components/loader';
+import Error from '../../components/error/error';
+
 import classes from './layout-page.module.css';
 
 import { useState , useEffect} from "react";
@@ -11,30 +13,55 @@ import { createNewData, createNewDataStars, options, sortRating, urlFilms, urlSt
 
 import { Context, visits, historyVisit } from '../../components/сontext';
 
-
 export default function LayoutPage() {
-    
+    // Стан пошукової строки.
     const [search, setSearch] = useState('');
-
+    // Стан масиву фільмів.
     const [filmsData, setFilmsData] = useState(null);
-    
+    // Стан масиву зірок.
     const [starsData, setStarsData] = useState(null);
-
+    // Стан замовлення.
     const [visitsInfo, setVisitsInfo] = useState(visits)
-    
+    // Стан історії замовлень.
     const [historyVisits, setHistoryVisits] = useState(historyVisit);
 
+    // Стан помилок.
+    const [error, setError] = useState(null);
+    // Стан лоудера.
+    const [loader, setLoader] = useState(false);
+
+    // Функція слідкує за помилками.
+    const filtrError = (res, flag) =>{
+        if(flag === 'films'){
+            if(res.results){
+                setFilmsData(sortRating(createNewData(res.results)))
+            }
+            else {
+                setError(res)
+                console.log(res)
+            }
+        }
+        if(flag === 'stars'){
+            if(res.results){
+                setStarsData(createNewDataStars(res.results))
+            }
+            else setError(res)
+        }
+        setLoader(false)
+    }
+ 
     // Робимо запит на сервер.
     useEffect(() => {
+        setLoader(true)
 
         fetch(urlFilms, options)
             .then(response => response.json())
-            .then(response => response.results && setFilmsData(sortRating(createNewData(response.results))))
+            .then(response => filtrError(response, 'films'))
             .catch(err => console.error(err));
             
         fetch(urlStars, options)
             .then(response => response.json())
-            .then(response => response.results && setStarsData(createNewDataStars(response.results)))
+            .then(response => filtrError(response, 'stars'))
             .catch(err => console.error(err));
     }, [])
 
@@ -69,10 +96,21 @@ export default function LayoutPage() {
 
                 <Context.Provider value={{ films:filmsData, stars:starsData,search:search, visitsInfo:visitsInfo,historyVisits:historyVisits, callbackSetSearchInput, callbackSetVisitsInfo, callbackSetHistoryVisits }}>
                     <Header/>
-                    <>
-                      {filmsData  ? <Outlet/>: <Loader/> }
-                    </>
-	
+                    <div className={classes.wrapper}>
+                      {error  
+                        ? 
+                      <Error text={error.status_message}/>
+                        :
+                        filmsData 
+                        && 
+                        starsData 
+                        && 
+                        <Outlet/>
+                        }
+                    </div>
+
+                    {loader && <Loader/>}
+
                     <button onClick={clickBtnTop} className={classes.btn_top}  type='button'><span>⇧</span></button>
 
                     <Footer/>
